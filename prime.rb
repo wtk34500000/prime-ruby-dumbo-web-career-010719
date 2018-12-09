e551b0b8c89c9a4d49221fe5248dc7f51c9cfcb6
recursive-certificate-9133 <wjhuang0726@gmail.com>
2018-12-09 05:25:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(number)
+  if number == 0 || number ==1
+    return false 
+  elsif number == 2 
+    return true 
+  else
+    previous_number = number -1
+    range =[2..previous_number]
+    numb_list =[]
+    
+    range.each do |num|
+      numb_list.collect(num)
+    end 
+    
+    index=0
+    while index < numb_list.length
+          if(number % numb_list[index]!=0)
+            index+=1
+          else
+            return true
+          
+    end
+  end
+  
+end
