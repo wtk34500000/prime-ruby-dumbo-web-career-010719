9db4096d618cc5b10e35fb23c03ae9a82dd66aa5
recursive-certificate-9133 <wjhuang0726@gmail.com>
2018-12-11 01:50:18 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+def prime?(n)
+  if n < 0 || n ==1 || n == 0
+    return false
+  elsif n==2 
+    return true
+  else
+    for d in 2..(n - 1)
+     if (n % d) == 0
+      return false
+     end
+    end
+    return true
+end
+end
